3739feb0e6d74a41192ae994f72bdfa0d332746e
MonkiMoyo <145808246+MonkiMoyo@users.noreply.github.com>
2023-10-07 15:22:51 +0000

    Omg carousel

--- app.js
+++ app.js
@@ -1,59 +1,100 @@
-const carousel = document.querySelector(".carousel"),
-firstImg = carousel.querySelectorAll("img")[0];
-arrowIcons = document.querySelectorAll(".wrapper .controls");
 
 
+class Slider{
+    constructor(slider){
+        this.sliderParent = slider
+        this.carousel = this.sliderParent.querySelector(".carousel"),
+        this.firstImg = this.carousel.querySelectorAll("img")[0];
+        this.arrowIcons = this.sliderParent.querySelectorAll(".wrapper .controls");
+        this.imgELs = this.sliderParent.querySelectorAll('.none-active');
+        this.changeImg = this.sliderParent.querySelectorAll('.controls');
 
-let isDragStart = false, prevPageX, prevScrollLeft;
-let firstImgWidth = firstImg.clientWidth + 14;
+        this.prevPageX
+        this.prevScrollLeft      
+        this.isDragStart = false, this.prevPageX, this.prevScrollLeft;
+        this.firstImgWidth = this.firstImg.clientWidth + 14;
+    }
 
 
-arrowIcons.forEach(icon => {
+    dragStart = (e) => {
+        this.isDragStart = true;
+        this.prevPageX = e.pageX || e.touches[0].pageX;
+        this.prevScrollLeft = this.carousel.scrollLeft;
+    }
+
+    dragging = (e) => {
+        if(!this.isDragStart) return;
+        e.preventDefault();
+        this.carousel.classList.add("dragging"); 
+        let positionDiff = (e.pageX || e.touches[0].pageX) - this.prevPageX; 
+        this.carousel.scrollLeft = this.prevScrollLeft - positionDiff;
+    }
+
+    dragStop = () => {
+        this.isDragStart = false;
+        this.carousel.classList.remove("dragging");
+    }
+
+}
+
+const sliderParent1 = document.getElementById("sliderParent1")
+const slider1 = new Slider(sliderParent1)
+
+slider1.arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        slider1.carousel.scrollLeft += icon.id == "left" ? -slider1.firstImgWidth : slider1.firstImgWidth;
         
     });
 });
 
-const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
 
-const dragging = (e) => {
-    if(!isDragStart) return;
-    e.preventDefault();
-    carousel.classList.add("dragging"); 
-    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX; 
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
+slider1.carousel.addEventListener("mousedown", slider1.dragStart);
+slider1.carousel.addEventListener("touchstart", slider1.dragStart);
 
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-}
+slider1.carousel.addEventListener("mousemove", slider1.dragging);
+slider1.carousel.addEventListener("touchmove", slider1.dragging);
 
+slider1.carousel.addEventListener("mouseup", slider1.dragStop);
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
+slider1.carousel.addEventListener("mouseleave", slider1.dragStop);
+slider1.carousel.addEventListener("touchend", slider1.dragStop);
 
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-
-carousel.addEventListener("mouseup", dragStop);
-
-carousel.addEventListener("mouseleave", dragStop);
-carousel.addEventListener("touchend", dragStop);
-
-const imgELs = document.querySelectorAll('.none-active');
-const changeImg = document.querySelectorAll('.controls');
-
-    imgELs.forEach(imgELs => {
+slider1.imgELs.forEach(imgELs => {
     imgELs.addEventListener('click', () => {
-    document.querySelector('.active')?.classList.remove('active');
+        console.log("click1");
+        slider1.sliderParent.querySelector('.active')?.classList.remove('active');
     imgELs.classList.add('active')   
-   });
+});
 }); 
 
 
+
+const sliderParent2 = document.getElementById("sliderParent2")
+const slider2 = new Slider(sliderParent2)
+
+slider2.arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        slider2.carousel.scrollLeft += icon.id == "left" ? -slider2.firstImgWidth : slider2.firstImgWidth;
+        
+    });
+});
+
+
+slider2.carousel.addEventListener("mousedown", slider2.dragStart);
+slider2.carousel.addEventListener("touchstart", slider2.dragStart);
+
+slider2.carousel.addEventListener("mousemove", slider2.dragging);
+slider2.carousel.addEventListener("touchmove", slider2.dragging);
+
+slider2.carousel.addEventListener("mouseup", slider2.dragStop);
+
+slider2.carousel.addEventListener("mouseleave", slider2.dragStop);
+slider2.carousel.addEventListener("touchend", slider2.dragStop);
+
+slider2.imgELs.forEach(imgELs => {
+    imgELs.addEventListener('click', () => {
+        console.log("click2");
+        slider2.sliderParent.querySelector('.active')?.classList.remove('active');
+    imgELs.classList.add('active')   
+});
+}); 
