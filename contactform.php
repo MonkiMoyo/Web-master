7c62bf9b7cf2507e34216719e61b6dc062cc1480
MonkiMoyo <145808246+MonkiMoyo@users.noreply.github.com>
2023-10-15 21:09:20 +0000

    php änderung

--- contactform.php
+++ contactform.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit'])) {
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
 
-mail($mailTo, $txt, $headers);
+mail($mailTo, $headers, $txt);
 header("Location: kontakt.html?mailsend");
 }
 
